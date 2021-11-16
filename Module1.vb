98f8fc63294fc0e553c4f6caa47b2e584e81ca37
AamnaSaleem <93542393+AamnaSaleem@users.noreply.github.com>
2021-11-16 21:02:48 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, FirstChars, LastChars As String
+        
+        Console.Write(" Enter string to be processed: ")
+        Str1 = Console.ReadLine
+
+        If Len(Str1) <= 2 Then
+            Console.WriteLine(Str1)
+            Console.ReadKey()
+            End
+        End If
+
+
+        FirstChars = Left(Str1, 2)
+        LastChars = Right(Str1, 2)
+
+
+
+        Console.WriteLine("Processed string is: " & FirstChars & LastChars)
+        Console.ReadKey()
+    End Sub
+
+End Module
